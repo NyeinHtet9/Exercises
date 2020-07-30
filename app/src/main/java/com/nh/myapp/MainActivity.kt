d598f7674595a2e55d0dd37b551b86db6a38864d
Nyein Htet <nyein.htet.lmb@gmail.com>
2020-07-30 15:04:25 +0630

    .

---
+++ app/src/main/java/com/nh/myapp/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.nh.myapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.oeschinen)
+    }
+
+
+
+}
